66d550ff7befb43ed90addebdb802425ead5df96
Hannes Herrmann <hannes.herrmann@tngtech.com>
2019-03-25 23:58:58 +0100

    generalize over holiday type

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use chrono::{NaiveDate, Datelike};
 use chrono::Duration;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-enum Holidays {
+enum GermanHolidays {
     Neujahr,
     HeiligeDreiKoenige,
     Frauentag,
@@ -37,29 +37,33 @@ fn bus_und_bettag(year: i32) -> NaiveDate {
     panic!("unimplemented")
 }
 
-impl Holidays {
+trait Holiday {
+    fn to_date(&self, year: i32) -> NaiveDate;
+}
+
+impl Holiday for GermanHolidays {
     fn to_date(&self, year: i32) -> NaiveDate {
         match self {
-            Holidays::Neujahr => date(year, 1, 1),
-            Holidays::HeiligeDreiKoenige => date(year, 1, 6),
-            Holidays::Frauentag => date(year, 1, 8),
-            Holidays::Karfreitag => oster_sonntag(year) - Duration::days(2),
-            Holidays::Ostersonntag => oster_sonntag(year),
-            Holidays::Ostermontag => oster_sonntag(year) + Duration::days(1),
-            Holidays::ErsterMai => date(year, 5, 1),
-            Holidays::ChristiHimmelfahrt => oster_sonntag(year) + Duration::days(39),
-            Holidays::Pfingstsonntag => oster_sonntag(year) + Duration::days(49),
-            Holidays::Pfingstmontag => oster_sonntag(year) + Duration::days(50),
-            Holidays::Fronleichnam => oster_sonntag(year) + Duration::days(60),
-            Holidays::AugsburgerFriedensfest => date(year, 8, 8),
-            Holidays::MariaeHimmelfahrt => date(year, 8, 15),
-            Holidays::Weltkindertag => date(year, 9, 20),
-            Holidays::TagDerDeutschenEinheit => date(year, 10, 3),
-            Holidays::Reformationstag => date(year, 10, 31),
-            Holidays::Allerheiligen => date(year, 11, 1),
-            Holidays::BussUndBettag => bus_und_bettag(year),
-            Holidays::ErsterWeihnachtsfeiertag => date(year, 12, 25),
-            Holidays::ZweiterWeihnachtsfeiertag => date(year, 12, 26),
+            GermanHolidays::Neujahr => date(year, 1, 1),
+            GermanHolidays::HeiligeDreiKoenige => date(year, 1, 6),
+            GermanHolidays::Frauentag => date(year, 1, 8),
+            GermanHolidays::Karfreitag => oster_sonntag(year) - Duration::days(2),
+            GermanHolidays::Ostersonntag => oster_sonntag(year),
+            GermanHolidays::Ostermontag => oster_sonntag(year) + Duration::days(1),
+            GermanHolidays::ErsterMai => date(year, 5, 1),
+            GermanHolidays::ChristiHimmelfahrt => oster_sonntag(year) + Duration::days(39),
+            GermanHolidays::Pfingstsonntag => oster_sonntag(year) + Duration::days(49),
+            GermanHolidays::Pfingstmontag => oster_sonntag(year) + Duration::days(50),
+            GermanHolidays::Fronleichnam => oster_sonntag(year) + Duration::days(60),
+            GermanHolidays::AugsburgerFriedensfest => date(year, 8, 8),
+            GermanHolidays::MariaeHimmelfahrt => date(year, 8, 15),
+            GermanHolidays::Weltkindertag => date(year, 9, 20),
+            GermanHolidays::TagDerDeutschenEinheit => date(year, 10, 3),
+            GermanHolidays::Reformationstag => date(year, 10, 31),
+            GermanHolidays::Allerheiligen => date(year, 11, 1),
+            GermanHolidays::BussUndBettag => bus_und_bettag(year),
+            GermanHolidays::ErsterWeihnachtsfeiertag => date(year, 12, 25),
+            GermanHolidays::ZweiterWeihnachtsfeiertag => date(year, 12, 26),
         }
     }
 }
@@ -83,82 +87,82 @@ enum Germany {
     Thueringen,
 }
 
-const BUNDESWEITE_FEIERTAGE: &'static [Holidays] = &[
-    Holidays::Neujahr,
-    Holidays::Karfreitag,
-    Holidays::Ostersonntag,
-    Holidays::Ostermontag,
-    Holidays::ErsterMai,
-    Holidays::ChristiHimmelfahrt,
-    Holidays::Pfingstsonntag,
-    Holidays::Pfingstmontag,
-    Holidays::TagDerDeutschenEinheit,
-    Holidays::ErsterWeihnachtsfeiertag,
-    Holidays::ZweiterWeihnachtsfeiertag,
+const BUNDESWEITE_FEIERTAGE: &'static [GermanHolidays] = &[
+    GermanHolidays::Neujahr,
+    GermanHolidays::Karfreitag,
+    GermanHolidays::Ostersonntag,
+    GermanHolidays::Ostermontag,
+    GermanHolidays::ErsterMai,
+    GermanHolidays::ChristiHimmelfahrt,
+    GermanHolidays::Pfingstsonntag,
+    GermanHolidays::Pfingstmontag,
+    GermanHolidays::TagDerDeutschenEinheit,
+    GermanHolidays::ErsterWeihnachtsfeiertag,
+    GermanHolidays::ZweiterWeihnachtsfeiertag,
 ];
 
 impl Germany {
-    fn holidays(&self) -> impl Iterator<Item=Holidays> {
+    fn holidays(&self) -> impl Iterator<Item=GermanHolidays> {
         BUNDESWEITE_FEIERTAGE.iter().cloned()
             .chain(self.region_specific_holidays().iter().cloned())
     }
 
-    fn region_specific_holidays(&self) -> &'static [Holidays] {
+    fn region_specific_holidays(&self) -> &'static [GermanHolidays] {
         match self {
             Germany::BadenWuerttemberg =>
-                &[Holidays::HeiligeDreiKoenige,
-                    Holidays::Fronleichnam,
-                    Holidays::Allerheiligen],
+                &[GermanHolidays::HeiligeDreiKoenige,
+                    GermanHolidays::Fronleichnam,
+                    GermanHolidays::Allerheiligen],
             Germany::Bayern =>
-                &[Holidays::HeiligeDreiKoenige,
-                    Holidays::Fronleichnam,
-                    Holidays::MariaeHimmelfahrt,
-                    Holidays::Allerheiligen],
-            Germany::Berlin => &[Holidays::Frauentag],
-            Germany::Brandenburg => &[Holidays::Reformationstag],
-            Germany::Bremen => &[Holidays::Reformationstag],
-            Germany::Hamburg => &[Holidays::Reformationstag],
-            Germany::Hessen => &[Holidays::Fronleichnam],
-            Germany::MechlenburgVorpommern => &[Holidays::Reformationstag],
-            Germany::Niedersachsen => &[Holidays::Reformationstag],
-            Germany::NordrheinWestfalen => &[Holidays::Fronleichnam, Holidays::Allerheiligen],
-            Germany::RheinlandPfalz => &[Holidays::Fronleichnam, Holidays::Allerheiligen],
+                &[GermanHolidays::HeiligeDreiKoenige,
+                    GermanHolidays::Fronleichnam,
+                    GermanHolidays::MariaeHimmelfahrt,
+                    GermanHolidays::Allerheiligen],
+            Germany::Berlin => &[GermanHolidays::Frauentag],
+            Germany::Brandenburg => &[GermanHolidays::Reformationstag],
+            Germany::Bremen => &[GermanHolidays::Reformationstag],
+            Germany::Hamburg => &[GermanHolidays::Reformationstag],
+            Germany::Hessen => &[GermanHolidays::Fronleichnam],
+            Germany::MechlenburgVorpommern => &[GermanHolidays::Reformationstag],
+            Germany::Niedersachsen => &[GermanHolidays::Reformationstag],
+            Germany::NordrheinWestfalen => &[GermanHolidays::Fronleichnam, GermanHolidays::Allerheiligen],
+            Germany::RheinlandPfalz => &[GermanHolidays::Fronleichnam, GermanHolidays::Allerheiligen],
             Germany::Saarland =>
-                &[Holidays::Fronleichnam, Holidays::MariaeHimmelfahrt, Holidays::Allerheiligen],
-            Germany::Sachsen => &[Holidays::Reformationstag, Holidays::BussUndBettag],
-            Germany::SachsenAnhalt => &[Holidays::HeiligeDreiKoenige, Holidays::Reformationstag],
-            Germany::SchleswigHolstein => &[Holidays::Reformationstag],
-            Germany::Thueringen => &[Holidays::Weltkindertag, Holidays::Reformationstag],
+                &[GermanHolidays::Fronleichnam, GermanHolidays::MariaeHimmelfahrt, GermanHolidays::Allerheiligen],
+            Germany::Sachsen => &[GermanHolidays::Reformationstag, GermanHolidays::BussUndBettag],
+            Germany::SachsenAnhalt => &[GermanHolidays::HeiligeDreiKoenige, GermanHolidays::Reformationstag],
+            Germany::SchleswigHolstein => &[GermanHolidays::Reformationstag],
+            Germany::Thueringen => &[GermanHolidays::Weltkindertag, GermanHolidays::Reformationstag],
         }
     }
 }
 
-trait Region {
+trait Region<H> where H: Holiday {
     fn is_holiday(&self, date: NaiveDate) -> bool;
-    fn holiday_from_date(&self, date: NaiveDate) -> Option<Holidays>;
+    fn holiday_from_date(&self, date: NaiveDate) -> Option<H>;
 }
 
-impl Region for Germany {
+impl Region<GermanHolidays> for Germany {
     fn is_holiday(&self, date: NaiveDate) -> bool {
         self.holidays()
             .any(|holiday| holiday.to_date(date.year()) == date)
     }
-    fn holiday_from_date(&self, date: NaiveDate) -> Option<Holidays> {
+    fn holiday_from_date(&self, date: NaiveDate) -> Option<GermanHolidays> {
         self.holidays().find(|holiday| holiday.to_date(date.year()) == date)
     }
 }
 
 
-trait IntoHoliday<T> where T: Region {
+trait IntoHoliday<T, H> where H: Holiday, T: Region<H> {
     fn is_holiday(&self, region: T) -> bool;
-    fn holiday(&self, region: T) -> Option<Holidays>;
+    fn holiday(&self, region: T) -> Option<H>;
 }
 
-impl<T: Region> IntoHoliday<T> for NaiveDate {
+impl<T, H> IntoHoliday<T, H> for NaiveDate where H: Holiday, T: Region<H> {
     fn is_holiday(&self, region: T) -> bool {
         region.is_holiday(*self)
     }
-    fn holiday(&self, region: T) -> Option<Holidays> {
+    fn holiday(&self, region: T) -> Option<H> {
         region.holiday_from_date(*self)
     }
 }
@@ -166,12 +170,12 @@ impl<T: Region> IntoHoliday<T> for NaiveDate {
 #[cfg(test)]
 mod tests {
     use chrono::NaiveDate;
-    use crate::{IntoHoliday, Germany, Holidays};
+    use crate::{IntoHoliday, Germany, GermanHolidays};
 
     #[test]
     fn neujahr_feiertag_in_bayern() {
         let date = NaiveDate::from_ymd(2018, 01, 01);
         assert!(date.is_holiday(Germany::Bayern));
-        assert_eq!(date.holiday(Germany::Bayern), Some(Holidays::Neujahr))
+        assert_eq!(date.holiday(Germany::Bayern), Some(GermanHolidays::Neujahr))
     }
 }
