8915f820c9552fdfbfc5a723ad020534dc9c37bc
Hannes Herrmann <hannes.herrmann@tngtech.com>
2019-05-10 14:09:12 +0200

    remove unnecessary HolidayRegion trait

--- src/germany.rs
+++ src/germany.rs
@@ -1,4 +1,4 @@
-use crate::{date, relative_to_easter_sunday, HolidayRegion};
+use crate::{date, relative_to_easter_sunday};
 use chrono::{Datelike, Duration, NaiveDate};
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -45,8 +45,8 @@ pub enum Germany {
 use crate::germany::GermanHolidays::*;
 use crate::germany::Germany::*;
 
-impl HolidayRegion for Germany {
-    fn holidays_in_year(&self, year: i32) -> Vec<GermanHolidays> {
+impl Germany {
+    pub fn holidays_in_year(&self, year: i32) -> Vec<GermanHolidays> {
         if year < 1995 {
             return Vec::new();
         }
@@ -83,6 +83,26 @@ impl HolidayRegion for Germany {
         };
         holidays.extend_from_slice(region_specific_holidays);
         holidays
+    }
+
+    pub fn holiday_dates_in_year(&self, year: i32) -> Vec<(NaiveDate, GermanHolidays)> {
+        let mut holiday_dates: Vec<(NaiveDate, GermanHolidays)> = self
+            .holidays_in_year(year)
+            .into_iter()
+            .flat_map(|holiday| holiday.to_date(year).map(|date| (date, holiday)))
+            .collect();
+        holiday_dates.sort_unstable_by_key(|(date, _)| *date);
+        holiday_dates
+    }
+
+    pub fn is_holiday(&self, date: NaiveDate) -> bool {
+        self.holiday_from_date(date).is_some()
+    }
+
+    pub fn holiday_from_date(&self, date: NaiveDate) -> Option<GermanHolidays> {
+        self.holidays_in_year(date.year())
+            .into_iter()
+            .find(|holiday| holiday.to_date(date.year()) == Some(date))
     }
 }
 
@@ -161,7 +181,7 @@ mod tests {
     use crate::germany::GermanHolidays::*;
     use crate::germany::Germany::*;
     use crate::germany::{bus_und_bettag, Germany};
-    use crate::{date, HolidayRegion, ToHoliday};
+    use crate::{date, ToHoliday};
     use chrono::{Datelike, NaiveDate, Weekday};
     use proptest::prelude::*;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use chrono::{Datelike, Duration, NaiveDate};
+use chrono::{Duration, NaiveDate};
 use computus;
 
 pub mod germany;
@@ -13,27 +13,6 @@ fn relative_to_easter_sunday(year: i32, days_offset: i64) -> Option<NaiveDate> {
     let easter_sunday = computus::gregorian(year).ok()?;
     let date = NaiveDate::from_ymd_opt(easter_sunday.year, easter_sunday.month, easter_sunday.day)?;
     Some(date + Duration::days(days_offset))
-}
-
-trait HolidayRegion {
-    fn holidays_in_year(&self, year: i32) -> Vec<GermanHolidays>;
-    fn holiday_dates_in_year(&self, year: i32) -> Vec<(NaiveDate, GermanHolidays)> {
-        let mut holiday_dates: Vec<(NaiveDate, GermanHolidays)> = self
-            .holidays_in_year(year)
-            .into_iter()
-            .flat_map(|holiday| holiday.to_date(year).map(|date| (date, holiday)))
-            .collect();
-        holiday_dates.sort_unstable_by_key(|(date, _)| *date);
-        holiday_dates
-    }
-    fn is_holiday(&self, date: NaiveDate) -> bool {
-        self.holiday_from_date(date).is_some()
-    }
-    fn holiday_from_date(&self, date: NaiveDate) -> Option<GermanHolidays> {
-        self.holidays_in_year(date.year())
-            .into_iter()
-            .find(|holiday| holiday.to_date(date.year()) == Some(date))
-    }
 }
 
 trait ToHoliday {
